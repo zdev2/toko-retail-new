1f3206e95134e21d036e633486b9480d30afcc7c
zdev2 <bagusperwira2020@gmail.com>
2025-01-24 12:04:25 +0800

    add somee model

--- domain/model.go
+++ domain/model.go
@@ -23,5 +23,38 @@ type Admin struct {
 type Product struct {
 	ID 		  	primitive.ObjectID 	`json:"_id"          bson:"_id"`
 	ProductName string 				`json:"product_name" bson:"product_name"`
+	Price       float64 			`json:"price"        bson:"price"`
 	Stock 		int 				`json:"stock"        bson:"stock"`
+	CreatedBy	string				`json:"created_by"   bson:"created_by"`
+	Base
+}
+
+type History struct {
+	ID 		  	primitive.ObjectID 	`json:"_id"          bson:"_id"`
+	ProductID	primitive.ObjectID  `json:"product_id"   bson:"product_id"`
+	Amount		int					`json:"amount"       bson:"amount"`
+	Status		string				`json:"status"       bson:"status"`
+	Description string				`json:"description"  bson:"description"`
+	Base
+}
+
+type Sale struct {
+	ID 		  		primitive.ObjectID 	`json:"_id"           bson:"_id"`
+	InvoiceCode		string				`json:"invoice_code"  bson:"invoice_code"`
+	BuyerName		string				`json:"buyer_name"    bson:"buyer_name"`
+	Subtotal		float64				`json:"subtotal"      bson:"subtotal"`
+	DiscountCode	string				`json:"discount_code" bson:"discount_code"`
+	Discount 		float64				`json:"discount"      bson:"discount"`
+	Total			float64				`json:"total"         bson:"total"`
+	CreatedBy		string				`json:"created_by"    bson:"created_by"`
+	Base
+	Items 			[]SaleItems			`json:"items"         bson:"items"`
+}
+
+type SaleItems struct {
+	ProductID	primitive.ObjectID `json:"product_id" bson:"product_id"`
+	SaleID		primitive.ObjectID `json:"sale_id"    bson:"sale_id"`
+	Amount		int				   `json:"amount"     bson:"amount"`
+	Subtotal	float64			   `json:"subtotal"   bson:"subtotal"`
+	Base
 }
